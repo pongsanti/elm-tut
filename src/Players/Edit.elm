75da8b59b3619500dbb627df3c13423f07fe8466
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-07-08 00:17:43 +0700

    navigation between pages

--- src/Players/Edit.elm
+++ src/Players/Edit.elm
@@ -4,6 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (class, value, href)
 import Msgs exposing (Msg)
 import Models exposing (Player)
+import Routing exposing (playersPath)
 
 
 view : Player -> Html Msg
@@ -17,7 +18,7 @@ view model =
 nav : Player -> Html Msg
 nav model =
     div [ class "clearfix mb2 white bg-black p1" ]
-        []
+        [ listBtn ]
 
 
 form : Player -> Html Msg
@@ -50,3 +51,14 @@ btnLevelIncrease : Player -> Html Msg
 btnLevelIncrease player =
     a [ class "btn ml1 h1" ]
         [ i [ class "fa fa-plus-circle" ] [] ]
+
+
+listBtn : Html Msg
+listBtn =
+    a
+        [ class "btn regular"
+        , href playersPath
+        ]
+        [ i [ class "fa fa-chevron-left mr1" ] []
+        , text "List"
+        ]
